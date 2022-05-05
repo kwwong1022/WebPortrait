5c1b935dfbc3a38d947a11e51944f5bb19f206bb
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2022-05-05 14:14:34 +0800

    v 0.0.5

--- background.js
+++ background.js
@@ -3,36 +3,34 @@ chrome.runtime.onInstalled.addListener(() => {
     console.log('Installation successfully completed!\nWeb Portrait extension\n- created by Wong Kai Fung');
 });
 
-// let jsn;
+// listen for http request update
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    if (changeInfo.url) {
+        // get updated url -> save to queue -> analyze @process-history.js: analysis web, load screenshot of pages
+        console.log(`url updates: ${changeInfo.url}, access date: ${(new Date()).getDate()}/${(new Date()).getMonth()}`);
+        // data structure: chrome.storage.local - name: historyQueue
+        // { isEmpty: bool, queue: array[...{url, dateAccess}] }
 
-// fetch('./manifest.json')
-// .then(res => {
-//     return res.json();
-// })
-// .then(jsonData => {
-//     jsn = JSON.stringify(jsonData);
-//     let blob = new Blob([jsn], { type: 'application/json' });
-//     let file = new File([ blob ], 'file.json');
-// });
+        chrome.storage.local.get(['historyQueue'], function(result) {
+            let temp = result.historyQueue;
+            if (!temp) {
+                // if history queue == undefined -> create 
+                console.log("history queue is empty");
+                chrome.storage.local.set({ historyQueue: { isEmpty: false, queue: [{ url: changeInfo.url, date: `${(new Date()).getDate()}-${(new Date()).getMonth()}-${(new Date()).getFullYear()}` }] } }, function() {console.log("history queue has been created")});
+                
+            } else {
+                temp.isEmpty = false;
+                temp.queue.push({ 
+                    url: changeInfo.url, 
+                    date: `${(new Date()).getDate()}-${(new Date()).getMonth()}-${(new Date()).getFullYear()}` 
+                });
+            }
+            chrome.storage.local.set({ historyQueue: temp }, function() { console.log("history queue has been updated") });
+        })
+        chrome.storage.local.get(['historyQueue'], function(result) { console.log(result.historyQueue) });
+    }
+});
 
-// window.onload = () => {
-//     chrome.tabs.captureVisibleTab((dataUrl) => {
-//         console.log(dataUrl);
-//         chrome.downloads.download({
-//             filename: "download.jpg",
-//             url: dataUrl
-//         })
-//     })
-// }
-
-// setInterval(() => {
-
-//     chrome.tabs.captureVisibleTab((dataUrl) => {
-//         console.log(dataUrl);
-//         chrome.downloads.download({
-//             filename: "download.jpg",
-//             url: dataUrl
-//         })
-//     })
-
-// }, 50000);
+let initHistoryQueue = () => {
+    chrome.storage.local.remove(['historyQueue'], function(){})
+}
